9b85ffb2d41f7da1c1316da8971299f16c6133ec
AlonsoER <alonsoer2343@gmail.com>
2022-04-21 12:17:44 -0600

    Repository listo

---
+++ SistemaTaller.BackEnd.API/Repository/IMarcaVehiculosRepository.cs
@@ -0,0 +1,9 @@
+﻿using SistemaTaller.BackEnd.API.Models;
+using SistemaTaller.BackEnd.API.Repository.Actions;
+
+namespace SistemaTaller.BackEnd.API.Repository
+{
+    public interface IMarcaVehiculosRepository : IObtenerRepository<MarcaVehiculo, int>, IInsertarRepository<MarcaVehiculo>, IActualizarRepository<MarcaVehiculo>, IEliminarRepository<int>
+    {
+    }
+}
