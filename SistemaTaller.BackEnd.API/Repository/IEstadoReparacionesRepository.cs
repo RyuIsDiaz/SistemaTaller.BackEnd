b116f546a9dc401b4a188d4395448f64350e1f96
AlonsoER <alonsoer2343@gmail.com>
2022-04-21 12:25:21 -0600

    Repository listo

---
+++ SistemaTaller.BackEnd.API/Repository/IEstadoReparacionesRepository.cs
@@ -0,0 +1,9 @@
+﻿using SistemaTaller.BackEnd.API.Models;
+using SistemaTaller.BackEnd.API.Repository.Actions;
+
+namespace SistemaTaller.BackEnd.API.Repository
+{
+    public interface IEstadoReparacionesRepository : IObtenerRepository<EstadoReparacion, int>, IInsertarRepository<EstadoReparacion>, IActualizarRepository<EstadoReparacion>, IEliminarRepository<int>
+    {
+    }
+}
