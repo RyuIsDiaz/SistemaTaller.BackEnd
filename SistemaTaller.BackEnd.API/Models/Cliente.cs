255f0ed0624f2435de37e5db34dc2af19bcee0df
AlonsoER <alonsoer2343@gmail.com>
2022-04-20 21:31:26 -0600

    Sin error

---
+++ SistemaTaller.BackEnd.API/Models/Cliente.cs
@@ -0,0 +1,27 @@
+﻿namespace SistemaTaller.BackEnd.API.Models
+{
+    public class Cliente
+    {
+		public string Identificacion { get; set; }
+
+		public string Nombre { get; set; }
+
+		public string Apellidos { get; set; }
+
+		public string Telefono { get; set; }
+
+		public string Email { get; set; }
+
+		public string Direccion { get; set; }
+
+		public bool? Activo { get; set; }
+
+		public DateTime? FechaCreacion { get; set; }
+
+		public DateTime? FechaModificacion { get; set; }
+
+		public string CreadoPor { get; set; }
+
+		public string ModificadoPor { get; set; }
+	}
+}
