670872de5c5db70a554522c426b245a4cabb339a
AlonsoER <alonsoer2343@gmail.com>
2022-04-20 21:48:41 -0600

    Models y Dto(Cliente) listo

---
+++ SistemaTaller.BackEnd.API/Dtos/TallerDto.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SistemaTaller.BackEnd.API.Dtos
+{
+    public class TallerDto
+    {
+		[Required(ErrorMessage = "{0} es un campo obligatorio")]
+		[MaxLength(45, ErrorMessage = "{0} tiene que tener máximo {1} caracteres")]
+		public string Nombre { get; set; }
+
+		[Required(ErrorMessage = "{0} es un campo obligatorio")]
+		[MaxLength(15, ErrorMessage = "{0} tiene que tener máximo {1} caracteres")]
+		public string Telefono { get; set; }
+
+		[Required(ErrorMessage = "{0} es un campo obligatorio")]
+		[MaxLength(15, ErrorMessage = "{0} tiene que tener máximo {1} caracteres")]
+		public string Canton { get; set; }
+
+		[Required(ErrorMessage = "{0} es un campo obligatorio")]
+		[MaxLength(80, ErrorMessage = "{0} tiene que tener máximo {1} caracteres")]
+		public string Direccion { get; set; }
+
+		[Required(ErrorMessage = "{0} es un campo obligatorio")]
+		[MaxLength(30, ErrorMessage = "{0} tiene que tener máximo {1} caracteres")]
+		public string Email { get; set; }
+
+		public bool? Activo { get; set; }
+	}
+}
