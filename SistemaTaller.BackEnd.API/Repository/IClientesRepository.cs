8a956bdf8780059458db57a4c1aee4adefe5c525
AlonsoER <alonsoer2343@gmail.com>
2022-04-21 12:06:26 -0600

    Repository listo

---
+++ SistemaTaller.BackEnd.API/Repository/IClientesRepository.cs
@@ -0,0 +1,9 @@
+﻿using SistemaTaller.BackEnd.API.Models;
+using SistemaTaller.BackEnd.API.Repository.Actions;
+
+namespace SistemaTaller.BackEnd.API.Repository
+{
+    public interface IClienteRepository : IObtenerRepository<Cliente, string>, IInsertarRepository<Cliente>, IActualizarRepository<Cliente>, IEliminarRepository<string>
+    {
+    }
+}
