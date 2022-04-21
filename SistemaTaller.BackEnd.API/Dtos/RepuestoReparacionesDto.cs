fda5abc674371b625274c62e65987695dcd27195
AlonsoER <alonsoer2343@gmail.com>
2022-04-20 22:42:25 -0600

    Dto

---
+++ SistemaTaller.BackEnd.API/Dtos/RepuestoReparacionesDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SistemaTaller.BackEnd.API.Dtos
+{
+    public class RepuestoReparacionesDto
+    {
+        [Required(ErrorMessage = "{0} es un campo obligatorio")]
+        public int IdReparaciones { get; set; }
+
+        [Required(ErrorMessage = "{0} es un campo obligatorio")]
+        [MaxLength(25, ErrorMessage = "{0} tiene que tener máximo {1} caracteres")]
+        public string CodigoRepuestos { get; set; }
+
+        public bool? Activo { get; set; }
+    }
+}
