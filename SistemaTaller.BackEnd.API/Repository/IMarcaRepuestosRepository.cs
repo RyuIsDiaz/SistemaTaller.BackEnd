724432b7038d9ced4478c82c0de658cb5691e4e6
AlonsoER <alonsoer2343@gmail.com>
2022-04-21 12:11:10 -0600

    Repository listo

---
+++ SistemaTaller.BackEnd.API/Repository/IMarcaRepuestosRepository.cs
@@ -0,0 +1,9 @@
+﻿using SistemaTaller.BackEnd.API.Models;
+using SistemaTaller.BackEnd.API.Repository.Actions;
+
+namespace SistemaTaller.BackEnd.API.Repository
+{
+    public interface IMarcaRepuestosRepository : IObtenerRepository<MarcaRepuesto, int>, IInsertarRepository<MarcaRepuesto>, IActualizarRepository<MarcaRepuesto>, IEliminarRepository<int>
+    {
+    }
+}
