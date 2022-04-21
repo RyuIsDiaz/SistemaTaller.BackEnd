2e866889e2c47d6dcd59bbde9a1510a97e25d290
AlonsoER <alonsoer2343@gmail.com>
2022-04-20 22:40:45 -0600

    Dto

---
+++ SistemaTaller.BackEnd.API/Dtos/ReparacionDto.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SistemaTaller.BackEnd.API.Dtos
+{
+    public class ReparacionDto
+    {
+		[Required(ErrorMessage = "{0} es un campo obligatorio")]
+		public DateTime FechasIngreso { get; set; }
+
+		[Required(ErrorMessage = "{0} es un campo obligatorio")]
+		public DateTime FechasSalida { get; set; }
+
+		[Required(ErrorMessage = "{0} es un campo obligatorio")]
+		[MaxLength(10, ErrorMessage = "{0} tiene que tener máximo {1} caracteres")]
+		public string PlacasVehiculos { get; set; }
+
+		[Required(ErrorMessage = "{0} es un campo obligatorio")]
+		[MaxLength(20, ErrorMessage = "{0} tiene que tener máximo {1} caracteres")]
+		public string IdMecanicos { get; set; }
+
+		[Required(ErrorMessage = "{0} es un campo obligatorio")]
+		[MaxLength(100, ErrorMessage = "{0} tiene que tener máximo {1} caracteres")]
+		public string DiagnosticosReparaciones { get; set; }
+
+		[Required(ErrorMessage = "{0} es un campo obligatorio")]
+		public int? IdEstadosReparacion { get; set; }
+
+		[Required(ErrorMessage = "{0} es un campo obligatorio")]
+		public decimal MontosDeObra { get; set; }
+
+		[Required(ErrorMessage = "{0} es un campo obligatorio")]
+		public decimal MontosRepuestos { get; set; }
+
+		public bool? Activo { get; set; }
+
+	}
+}
