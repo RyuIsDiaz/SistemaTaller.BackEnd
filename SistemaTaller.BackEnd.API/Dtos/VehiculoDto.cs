47a3ec61396c80644884f0cc2950b3fdb8539c3c
AlonsoER <alonsoer2343@gmail.com>
2022-04-20 22:26:17 -0600

    Dto

---
+++ SistemaTaller.BackEnd.API/Dtos/VehiculoDto.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SistemaTaller.BackEnd.API.Dtos
+{
+    public class VehiculoDto
+    {
+        [Required(ErrorMessage = "{0} es un campo obligatorio")]
+        [MaxLength(10, ErrorMessage = "{0} tiene que tener máximo {1} caracteres")]
+        public string Placa { get; set; }
+
+        [Required(ErrorMessage = "{0} es un campo obligatorio")]
+        public int? IdMarca { get; set; }
+
+        [Required(ErrorMessage = "{0} es un campo obligatorio")]
+        public int Modelo { get; set; }
+
+        public bool? Activo { get; set; }
+    }
+}
