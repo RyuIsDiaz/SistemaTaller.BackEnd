439c7327891d1a0acd400019bc1bedd14ea11c67
AlonsoER <alonsoer2343@gmail.com>
2022-04-20 22:35:54 -0600

    Dto

---
+++ SistemaTaller.BackEnd.API/Dtos/EstadoReparacionDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SistemaTaller.BackEnd.API.Dtos
+{
+    public class EstadoReparacionDto
+    {
+        [Required(ErrorMessage = "{0} es un campo obligatorio")]
+        [MaxLength(15, ErrorMessage = "{0} tiene que tener máximo {1} caracteres")]
+        public string Estados { get; set; }
+
+        public bool? Activo { get; set; }
+    }
+}
