c3ff0afeb11685659317db818874ca3c4e1647c4
AlonsoER <alonsoer2343@gmail.com>
2022-04-21 12:06:06 -0600

    Repository listo

---
+++ SistemaTaller.BackEnd.API/Repository/IMecanicosRepository.cs
@@ -0,0 +1,9 @@
+﻿using SistemaTaller.BackEnd.API.Models;
+using SistemaTaller.BackEnd.API.Repository.Actions;
+
+namespace SistemaTaller.BackEnd.API.Repository
+{
+    public interface IMecanicosRepository : IObtenerRepository<Mecanico, string>, IInsertarRepository<Mecanico>, IActualizarRepository<Mecanico>, IEliminarRepository<string>
+    {
+    }
+}
