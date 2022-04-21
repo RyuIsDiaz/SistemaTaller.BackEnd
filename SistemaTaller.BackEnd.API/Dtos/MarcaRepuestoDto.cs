d73fab9cdfed4c4b6db524bbf43d730f6ffd02db
AlonsoER <alonsoer2343@gmail.com>
2022-04-20 22:18:57 -0600

    Dtos

---
+++ SistemaTaller.BackEnd.API/Dtos/MarcaRepuestoDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SistemaTaller.BackEnd.API.Dtos
+{
+    public class MarcaRepuestoDto
+    {
+        [Required(ErrorMessage = "{0} es un campo obligatorio")]
+        [MaxLength(20, ErrorMessage = "{0} tiene que tener máximo {1} caracteres")]
+        public string Nombre { get; set; }
+
+        public bool? Activo { get; set; }
+
+    }
+}
