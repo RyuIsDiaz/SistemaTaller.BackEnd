d49ff5bf68dd803fadf304c84914cec5c49e7aab
AlonsoER <alonsoer2343@gmail.com>
2022-04-25 16:22:22 -0600

    Repositorios listos

--- SistemaTaller.BackEnd.API/UnitOfWork.SqlServer/UnitOfWorkSqlServerRepository.cs
+++ SistemaTaller.BackEnd.API/UnitOfWork.SqlServer/UnitOfWorkSqlServerRepository.cs
@@ -23,7 +23,7 @@ namespace SistemaTaller.BackEnd.API.UnitOfWork.SqlServer
 
         public UnitOfWorkSqlServerRepository(SqlConnection context, SqlTransaction transaction)
         {
-            //Acá van todos los otros repositorios.
+            //Acá van todos los otros repositorios..
             ClientesRepository = new ClienteRepository(context, transaction);
             EstadoReparacionesRepository = new EstadoReparacionesRepository(context, transaction);
             MarcaRepuestosRepository = new MarcaRepuestosRepository(context, transaction);
@@ -34,8 +34,8 @@ namespace SistemaTaller.BackEnd.API.UnitOfWork.SqlServer
             RepuestoReparacionesRepository = new RepuestoReparacionesRepository(context, transaction);
             RepuestosRepository = new RepuestosRepository(context, transaction);
             TalleresRepository = new TalleresRepository(context, transaction);
-            //VehiculosClienteRepository = new VehiculosClienteRepository(context, transaction);
-            //VehiculosRepository = new VehiculosRepository(context, transaction);
+            VehiculosClienteRepository = new VehiculosClienteRepository(context, transaction);
+            VehiculosRepository = new VehiculosRepository(context, transaction);
         }
 
     }
