cadb6c683bc31d55acfbba24d01f5c735b774fef
ryudiaz818@gmail.com <ryudiaz818@gmail.com>
2022-04-25 21:18:31 -0600

    8 Services listos

---
+++ SistemaTaller.BackEnd.API/Services/RepuestoReparacionesServices.cs
@@ -0,0 +1,73 @@
+﻿using SistemaTaller.BackEnd.API.Models;
+using SistemaTaller.BackEnd.API.Services.Interfaces;
+using SistemaTaller.BackEnd.API.UnitOfWork;
+
+namespace SistemaTaller.BackEnd.API.Services
+{
+    public class RepuestoReparacionesServices : IRepuestoReparacionesService
+    {
+        private IUnitOfWork BD;
+
+        public RepuestoReparacionesServices(IUnitOfWork unitOfWork)
+        {
+            BD = unitOfWork;
+        }
+        public void Actualizar(RepuestoReparacion model)
+        {
+            using (var bd = BD.Conectar())
+            {
+                bd.Repositories.RepuestoReparacionesRepository.Actualizar(model);
+
+                bd.SaveChanges();
+            }
+        }
+
+        public void Eliminar(int id)
+        {
+            using (var bd = BD.Conectar())
+            {
+                bd.Repositories.RepuestoReparacionesRepository.Eliminar(id);
+
+                bd.SaveChanges();
+            }
+        }
+
+        public void Insertar(RepuestoReparacion model)
+        {
+            using (var bd = BD.Conectar())
+            {
+                bd.Repositories.RepuestoReparacionesRepository.Insertar(model);
+
+                bd.SaveChanges();
+            }
+        }
+
+        public RepuestoReparacion SeleccionarPorId(int id)
+        {
+            RepuestoReparacion RepuestoReparacionSeleccionado = new();
+
+            using (var bd = BD.Conectar())
+            {
+                RepuestoReparacionSeleccionado = bd.Repositories.RepuestoReparacionesRepository.SeleccionarPorId(id);
+
+                bd.SaveChanges();
+            }
+
+            return RepuestoReparacionSeleccionado;
+        }
+
+        public List<RepuestoReparacion> SeleccionarTodos()
+        {
+            List<RepuestoReparacion> ListaTodosRepuestoReparacion;
+
+            using (var bd = BD.Conectar())
+            {
+                ListaTodosRepuestoReparacion = bd.Repositories.RepuestoReparacionesRepository.SeleccionarTodos();
+
+                bd.SaveChanges();
+            }
+
+            return ListaTodosRepuestoReparacion;
+        }
+    }
+}
