8978cf152a273bd503195e1c0c7381807b082b21
FABIAN JOSE VALERIN CRUZ <fabian.valerin@ulatina.net>
2022-04-27 15:49:33 -0600

    Cambios Fabián

--- SistemaTaller.BackEnd.API/Controllers/TalleresController.cs
+++ SistemaTaller.BackEnd.API/Controllers/TalleresController.cs
@@ -4,7 +4,7 @@ using SistemaTaller.BackEnd.API.Models;
 using SistemaTaller.BackEnd.API.Services.Interfaces;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
-
+//roy
 namespace SistemaTaller.BackEnd.API.Controllers
 {
     [Route("api/[controller]")]
