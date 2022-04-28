c9d833f9ca1a8e2457348f7ff4359569f5a41dd8
FABIAN JOSE VALERIN CRUZ <fabian.valerin@ulatina.net>
2022-04-28 16:49:43 -0600

    Arreglo Vehículo Controlador

--- SistemaTaller.BackEnd.API/Controllers/VehiculosController.cs
+++ SistemaTaller.BackEnd.API/Controllers/VehiculosController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SistemaTaller.BackEnd.API.Dtos;
+using SistemaTaller.BackEnd.API.Models;
 using SistemaTaller.BackEnd.API.Services.Interfaces;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -17,9 +19,23 @@ namespace SistemaTaller.BackEnd.API.Controllers
         }
         // GET: api/<VehiculosController>
         [HttpGet]
-        public IEnumerable<string> Get()
+public List<VehiculoDto> Get()
         {
-            return new string[] { "value1", "value2" };
+            List<Vehiculos> ListaTodosLosVehiculos = ServicioVehiculos.SeleccionarTodos();
+
+            List<VehiculoDto> ListaTodasLosVehiculosDto = new();
+
+            foreach (var VehiculosSeleccionado in ListaTodosLosVehiculos)
+            {
+                VehiculoDto VehiculosDTO = new();
+                VehiculosDTO.IdMarca = VehiculosSeleccionado.IdMarca;
+                VehiculosDTO.Placa = VehiculosSeleccionado.Placa;
+                VehiculosDTO.Modelo = VehiculosSeleccionado.Modelo;
+                VehiculosDTO.Activo = VehiculosSeleccionado.Activo;
+                ListaTodasLosVehiculosDto.Add(VehiculosDTO);
+            }
+
+            return ListaTodasLosVehiculosDto;
         }
 
         // GET api/<VehiculosController>/5
@@ -31,14 +47,67 @@ namespace SistemaTaller.BackEnd.API.Controllers
 
         // POST api/<VehiculosController>
         [HttpPost]
-        public void Post([FromBody] string value)
+public IActionResult Post([FromBody] VehiculoDto VehiculosDTO)
         {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    Vehiculo VehiculoPorInsertar = new();
+
+                    VehiculoPorInsertar.IdMarca = VehiculosDTO.IdMarca;
+                    VehiculoPorInsertar.Placa = VehiculosDTO.Placa;
+                    VehiculoPorInsertar.Modelo = VehiculosDTO.Modelo;
+                    VehiculoPorInsertar.Activo = VehiculosDTO.Activo;
+                    VehiculoPorInsertar.CreadoPor = "Fabián";
+                    ServicioVehiculos.Insertar(VehiculoPorInsertar);
+
+                    return Ok();
+                }
+                else
+                {
+                    string ErroreEnElModelo = ObtenerErroresDeModeloInvalido();
+
+                    return BadRequest(ErroreEnElModelo);
+                }
+            }
+            catch (Exception Ex)
+            {
+                return BadRequest(Ex.Message);
+            }
         }
 
         // PUT api/<VehiculosController>/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+public IActionResult Put(string id, [FromBody] VehiculoDto VehiculosDTO)
         {
+
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    Vehiculo VehiculoPorActualizar = new();
+                    VehiculoPorActualizar.IdMarca = VehiculosDTO.IdMarca;
+                    VehiculoPorActualizar.Placa = VehiculosDTO.Placa;
+                    VehiculoPorActualizar.Modelo = VehiculosDTO.Modelo;
+                    VehiculoPorActualizar.Activo = VehiculosDTO.Activo;
+                    VehiculoPorActualizar.ModificadoPor = "fabian";
+                    VehiculoPorActualizar.Activo = true;
+
+
+                    ServicioVehiculos.Actualizar(VehiculoPorActualizar);
+                    return Ok();
+                }
+                else
+                {
+                    string ErroreEnElModelo = ObtenerErroresDeModeloInvalido();
+                    return BadRequest(ErroreEnElModelo);
+                }
+            }
+            catch (Exception Ex)
+            {
+                return BadRequest(Ex.Message);
+            }
         }
 
         // DELETE api/<VehiculosController>/5
